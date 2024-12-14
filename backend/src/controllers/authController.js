84ec858384418bd992493ae2a42df229c8d87706
Julien Wangon <julien.wangon@gmail.com>
2024-12-14 20:48:49 +0100

    Modification de la réponse API pour connexion utilisateur réussie

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -39,7 +39,7 @@ async function login (req, res) {
         id: utilisateur.id,
         nom: utilisateur.nom,
         prenom: utilisateur.prenom,
-        email: utilisateur.email,
+        roleId: utilisateur.roleId,
       },
     });
 
