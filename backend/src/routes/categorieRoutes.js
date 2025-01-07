49cd53bb5aecddbdae5af9bf4d86eedfae453ba3
Julien Wangon <julien.wangon@gmail.com>
2025-01-07 22:07:04 +0100

    Ajout de la route pour la mise à jour d'une catégorie existante dans le fichier categorieRoutes.js

--- backend/src/routes/categorieRoutes.js
+++ backend/src/routes/categorieRoutes.js
@@ -20,16 +20,34 @@ const validateCreateCategorie = [
 
   body('description')
     .notEmpty()
-    .isLength({ max: 500 })
+    .isLength({ max: 250 })
     .withMessage('La description ne doit pas dépasser 250 caractères.'),
 ];
 
+
+// Règle de validation pour la mise à jour d"une catégorie
+const validateUpdateCategorie = [
+  body('nom')
+    .optional()
+    .matches(/^[A-Za-zÀ-ÖØ-öø-ÿ\s]+$/)
+    .withMessage('Le nom de la catégorie ne peut contenir que des lettres et des espaces.')
+    .isLength({ max: 50})
+    .withMessage('Le nom de la catégorie ne doit pas dépasser 50 caractères.'),
+
+  body('description')
+    .optional(),
+    .isLength({ max: 250 })
+    .withMessage('La description ne doit pas dépasser 250 caractères.'),
+]
 
 // Route pour récupérer toutes les catégories
 router.get('/', categorieController.getAllCategories);
 
 // Route pour créer une nouvelle catégorie
 router.post('/create', authMiddleware, isAdmin, validateCreateCategorie, categorieController.createCategorie);
+
+// Route pour mettre à jour une catégorie existante
+router.put('/:id/update', authMiddleware, isAdmin, validateUpdateCategorie, categorieController.updateCategorie);
 
 
 module.exports = router;
