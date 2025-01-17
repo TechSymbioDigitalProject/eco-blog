06f407e628941915c1d72ad367e8a0897aeaad36
Julien Wangon <julien.wangon@gmail.com>
2025-01-18 00:44:53 +0100

    Creation du controller pour les articles de la page d'accueil

---
+++ backend/src/controllers/articleController.js
@@ -0,0 +1,41 @@
+const Article = require('../models/Article');
+const logger = require('../config/logger');
+
+
+// Méthode poru récupérer les articles à afficher sur la page d'accueil
+async function getAllHomepageArticles(req, res) {
+  try {
+    const articles = await Article.getHomepageArticles();
+
+    const formattedArticles = articles.map(article => ({
+      id: article.id,
+      titre: article.titre,
+      image: article.main_image_url,
+      description: article.meta_description,
+      date: article.date_publication,
+    }));
+
+    logger.info('Liste des articles récupérée avec succès.', {
+        count: formattedArticles.length,
+    });
+
+    res.status(200).json({
+        message: 'Liste des articles récupérée avec succès.',
+        articles: formattedArticles,
+    });
+
+  } catch (err) {
+    logger.error('Erreur lors de la récupération des articles pour la page d\'accueil.', {
+      error: err.message,
+      stack: err.stack,
+    });
+
+    res.status(500).json({
+      message: 'Une erreur est survenue lors de la récupération des articles.',
+    });
+  }
+}
+
+module.exports = {
+  getAllHomepageArticles,
+};
