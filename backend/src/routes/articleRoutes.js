45995e6f453d41b7d99e1b5fd14291e94a10a3cb
Julien Wangon <julien.wangon@gmail.com>
2025-01-18 01:19:16 +0100

    Ajout de la route pour la pagination des articles de la page d'accueil publique

--- backend/src/routes/articleRoutes.js
+++ backend/src/routes/articleRoutes.js
@@ -7,5 +7,8 @@ const router = express.Router();
 // Route pour récupérer les articles pour la page d'accueil
 router.get('/homepage', articleController.getAllHomepageArticles);
 
+// Route pour la pagination des articles
+router.get('/paginated', articleController.getPaginatedArticles);
+
 
 module.exports = router;
