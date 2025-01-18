33b0e5999c0f9e34f0c02b71c345467c97302c78
Julien Wangon <julien.wangon@gmail.com>
2025-01-18 11:58:55 +0100

    Ajout de la fonction de traitement des images

--- backend/src/utils/imageUtils.js
+++ backend/src/utils/imageUtils.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const sharp = require('sharp');
+const logger = require('../config/logger');
 
 
 // Fonction pour la création d'un dossier pourun article si il n'existe pas 
@@ -9,8 +10,34 @@ function createArticleFolder(articleId) {
   
   if (!fs.existsSync(articleFolder)) {
     fs.mkdirSync(articleFolder, { recursive: true });
-  
+    logger.info(`Dossier créé : ${articleFolder}`); 
+  } else {
+    logger.info(`Dossier déjà existant : ${articleFolder}`);
   }
   
   return articleFolder;
 };
+
+
+// Fonction pour générer un nom de fichier unique pour une image
+function generateImageName(articleId, mediaId) {
+  return `image-article${articleId}-media${mediaId}.webp`;
+}
+
+// Fonction pour le traitement de l'image enregistrement au format webp et redimentionnement 800x800 maximum
+async function processImage(imageBuffer, destinationPath) {
+  try {
+    await sharp(imageBuffer)
+      .resize(800, 800, { fit: 'inside' }) // Redimensionne tout en maintenant les proportions
+      .toFormat('webp') // Convertit en WebP
+      .toFile(destinationPath);
+    
+    logger.info(`Image traitée et enregistrée : ${destinationPath}`);
+
+  } catch (error) {
+    logger.error('Erreur lors du traitement de l\'image :', { error: error.message });
+    
+    throw error;
+  }
+
+}
