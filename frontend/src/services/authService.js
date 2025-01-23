70aa506538899be92e7edfa9a05d18c2d00b297a
Julien Wangon <julien.wangon@gmail.com>
2025-01-23 21:22:24 +0100

    Ajout de la méthode checkSession au service authService front end

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -6,6 +6,7 @@ export const loginUser = async (email, password) => {
   try {
     const response = await api.post('/auth/login', { email, password });
     return response.data;
+
   } catch (error) {
     if(error.response && error.response.data) {
       throw new Error(error.response.data.message || 'erreur de connexion.');
@@ -14,3 +15,20 @@ export const loginUser = async (email, password) => {
     throw new Error('Erreur de connexion. Veuillez réessayer plus tard.');
   }
 };
+
+
+// Fonction pour contrôler la session utilisateur
+export const checkSession = async () => {
+  try {
+    const response = await api.get('/auth/check-session', { withCredentials: true });
+    return response.data;
+
+  } catch (error) {
+    if(error.response && error.response.status === 401) {
+      throw new Error('Session invalide ou expirée.');
+    }
+    throw new Error('Erreur lors de la vérification de la session.');
+  }
+};
+
+
