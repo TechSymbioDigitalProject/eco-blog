6f01c7c88aa2d08ef78bf88d770c25f1057b9d6a
Julien Wangon <julien.wangon@gmail.com>
2025-01-23 23:31:38 +0100

    Ajout de la vérification de connexion au context d authentification

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,6 +1,6 @@
 import { createContext } from 'preact';
-import { useState, useCallback } from 'preact/hooks';
-import { loginUser } from '../services/authService';
+import { useState, useCallback, useEffect } from 'preact/hooks';
+import { checkSession, loginUser } from '../services/authService';
 
 // Création du context
 // @ts-ignore
@@ -11,6 +11,7 @@ export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [user, setUser] = useState(null);
   const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
 
   // Fonction pour gérer la connexion
@@ -20,10 +21,34 @@ export const AuthProvider = ({ children }) => {
       const response = await loginUser(email, password);
       setIsAuthenticated(true);
       setUser(response.utilisateur);
+
     } catch (err) {
       setError(err.message);
     }
   }, [])
+
+
+  // Fonction pour vérifier la session existante 
+  const verifySession = useCallback(async () => {
+    try {
+      const userData = await checkSession();
+      setIsAuthenticated(true);
+      setUser(userData);
+
+    } catch (err) {
+      setIsAuthenticated(false);
+      setUser(null);
+
+    } finally {
+      setLoading(false)
+    }
+  }, [])
+
+
+  useEffect(() => {
+    verifySession();
+  }, [verifySession])
+
 
   // Fonction pour réinitialiser l'erreur
   const resetError = useCallback(() => {
@@ -33,7 +58,7 @@ export const AuthProvider = ({ children }) => {
 
   // Fournir les données et action dans le contexte
   return (
-    <AuthContext.Provider value={{ isAuthenticated, user, error, login, resetError }}>
+    <AuthContext.Provider value={{ isAuthenticated, user, error, loading, login, verifySession, resetError }}>
       {children}
     </AuthContext.Provider>
   );
