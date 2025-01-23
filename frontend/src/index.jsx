d249e7f022a201681f73c7cb7cf9e438b260e167
Julien Wangon <julien.wangon@gmail.com>
2025-01-23 23:31:13 +0100

    Modification du routage pour protéger les routes admin

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,14 +1,11 @@
 import { render } from 'preact';
 import './index.css';
-import { AuthProvider } from './context/AuthContext';
 import AppRoutes from './routes/AppRoutes';
 
 export function App() {
 	return (	
-		<AuthProvider>
-				<AppRoutes />
-		</AuthProvider>	
 		
+				<AppRoutes />	
 	);
 }
 
--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,16 +1,32 @@
 import { h } from "preact";
 import Router from "preact-router";
 import AdminLogin from "../pages/AdminLogin";
+import AdminHome from "../pages/AdminHome";
 import { AuthProvider } from "../context/AuthContext";
+import ProtectedRoute from "./ProtectedRoute";
 
-const AppRoutes = () => {
-  return (
-    <AuthProvider>
+const PublicRoutes = () => (
+  <Router>
+    
+  </Router>
+);
+
+const ProtectedRoutes = () => (
+  <AuthProvider>
     <Router>
       <AdminLogin path="/access-panel" />
+      <ProtectedRoute path="/admin-home">
+        <AdminHome />
+      </ProtectedRoute>
     </Router>
   </AuthProvider>
-  );
-};
+);
+
+const AppRoutes = () => (
+  <>
+    <PublicRoutes />
+    <ProtectedRoutes />
+  </>
+);
 
 export default AppRoutes;
